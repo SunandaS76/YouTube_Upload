dab8ed959b8e729793baa09c6c0c191b37cc9530
SunandaS76 <53965652+SunandaS76@users.noreply.github.com>
2019-11-12 20:16:47 +0530

    Update Code_youtube_upload.py

--- Code_youtube_upload.py
+++ Code_youtube_upload.py
@@ -1,19 +1,28 @@
-import sqlite3
+import os
+import time
 import xlrd
-import requests
+import random
+import httplib2
+import http.client
 import googleapiclient
 import google_auth_oauthlib
-import googleapiclient.discovery
-from googleapiclient.http import MediaFileUpload
-import os
+import googleapiclient.errors
 import google_auth_oauthlib.flow
 import googleapiclient.discovery
-import googleapiclient.errors
+from googleapiclient.errors import HttpError
+from googleapiclient.http import MediaFileUpload
 
 
+httplib2.RETRIES = 1
+MAX_RETRIES = 10
+RETRIABLE_EXCEPTIONS = (httplib2.HttpLib2Error, IOError,
+  http.client.IncompleteRead, http.client.ImproperConnectionState,
+  http.client.CannotSendRequest, http.client.CannotSendHeader,
+  http.client.ResponseNotReady, http.client.BadStatusLine)
+
+RETRIABLE_STATUS_CODES = [500, 502, 503, 504]
 
 scopes = ["https://www.googleapis.com/auth/youtube.upload"]
-
 
 
 class upload:
@@ -22,35 +31,44 @@ class upload:
         self.title_of_video = ""
         self.video_description = ""
         self.video_tags = ""
-        self.category_id = ""
+        self.category_id = int
         self.privacy_status = ""
         self.file_path_thumbnail = ""
         self.channel_id = ""
+        self.file_name = ""
         print("ENTER PATH OF FILE")
         self.path = input()
-        self.open_excel_file()
-        print(self.path)
+        self.open_excel_file(1)
+#        print(self.path)
 
 
-    def open_excel_file(self):
+    def open_excel_file(self,start):
         loc = str(self.path)
+        self.start=start
         wb = xlrd.open_workbook(loc)
         sheet = wb.sheet_by_index(0)
         column = sheet.ncols
         rows = sheet.nrows
+        print(rows,column)
         youtube = self.authorization()
-        for j in range(0,column):
-            for i in range(1,rows):
+        for i in range(start,rows):
+            for j in range(0,column):
                 self.video1_file_path = sheet.cell_value(i,0)
                 self.title_of_video = sheet.cell_value(i,1)
                 self.video_description = sheet.cell_value(i,2)
                 self.video_tags = sheet.cell_value(i,3)
-                self.category_id = sheet.cell_value(i,4)
+                self.category_id =int(sheet.cell_value(i,4))
                 self.privacy_status = sheet.cell_value(i,5)
                 self.file_path_thumbnail = sheet.cell_value(i,6)
                 self.channel_id = sheet.cell_value(i,7)
-                print(sheet.cell_value(i,j))
-                self.initialize_upload(youtube)
+                self.file_name = sheet.cell_value(i,8)
+                #print(sheet.cell_value(i,j))
+            self.initialize_upload(youtube)
+        #    self.resumable_upload(self.insert_request)
+        if i<rows:
+            open_excel_file(i)
+        else :
+            print('uploaded')
 
     def authorization(self) :
         # Disable OAuthlib's HTTPS verification when running locally.
@@ -59,7 +77,7 @@ class upload:
 
         api_service_name = "youtube"
         api_version = "v3"
-        client_secrets_file = "client_credentials.json"
+        client_secrets_file = r"C:\Users\Devhuti\Desktop\client_credentials3.json"
 
         # Get credentials and create an API client
         flow = google_auth_oauthlib.flow.InstalledAppFlow.from_client_secrets_file(
@@ -69,22 +87,28 @@ class upload:
         api_service_name, api_version, credentials=credentials)
 
     def initialize_upload(self,youtube):
-        body=dict(
+        self.body=dict(
         snippet = dict(
         title=self.title_of_video,
         description=self.video_description,
         tags=self.video_tags,
         categoryId=self.category_id,
-        file1 = self.video1_file_path,
-        channelid = self.channel_id
+        file = self.video1_file_path,
+        channelid = self.channel_id,
         ),
         status=dict(
         privacyStatus=self.privacy_status
+        ),
+        fileDetails=dict(
+        fileName = self.file_name)
         )
-        )
-        insert_request = youtube.videos().insert(part=",".join(body.keys()),
-            body=body,
+        self.insert_request = youtube.videos().insert(part=",".join(self.body.keys()),
+        body=self.body,
         media_body=MediaFileUpload(self.video1_file_path,chunksize=-1, resumable=True))
+        #media_body.
+        self.insert_request.execute()
+        
 
-
+    
 ud = upload()
+
